ad85ee1f9ce181630679cb9bf73eb0bb56c16ddd
nickyparekh <nicky1956@gmail.com>
2025-09-25 14:10:57 -0400

    finished ui interactions

--- discord_bot.py
+++ discord_bot.py
@@ -12,8 +12,8 @@ GUILD_ID = 1161391779845783782 #niccy's server
 intents = discord.Intents.default()
 bot = commands.Bot(intents=intents, command_prefix=None)
 
-# --- Dropdown setup ---
-class CategorySelect(discord.ui.Select):
+#Dropdown setup
+class CourseSelect(discord.ui.Select):
     def __init__(self, date, start_dt, end_dt):
         options = []
         for course in const.foreUp_mappings.keys():
@@ -30,24 +30,20 @@ class CategorySelect(discord.ui.Select):
         self.end_dt = end_dt
 
     async def callback(self, interaction: discord.Interaction):
-        courses = ", ".join(self.values)
-        await interaction.response.send_message(
-            f"📅 Date: **{self.date.strftime('%B %d, %Y')}**\n"
-            f"🕒 Start: **{self.start_dt.strftime('%H:%M')}**\n"
-            f"🕒 End: **{self.end_dt.strftime('%H:%M')}**\n"
-            f"⛳️ Courses: **{courses}**",
-            ephemeral=True
-        )
+        self.view.selected_courses = self.values
+        await interaction.response.send_message("Received.")
+        self.view.stop()
 
-class CategoryView(discord.ui.View):
+class CourseView(discord.ui.View):
     def __init__(self, date, start_dt, end_dt):
         super().__init__(timeout=60)
-        self.add_item(CategorySelect(date, start_dt, end_dt))
+        self.add_item(CourseSelect(date, start_dt, end_dt))
+        self.selected_courses: list[str] = []
 
 # --- Slash command ---
 @bot.tree.command(
     name="find",
-    description="Find something by date, time range, and categories",
+    description="Find golf tee times",
     guild=discord.Object(id=GUILD_ID)
 )
 @app_commands.describe(
@@ -59,8 +55,10 @@ async def find(
     interaction: discord.Interaction,
     date: str,
     start_time: str,
-    end_time: str
+    end_time: str,
+    players: int
 ):
+    #Initial Command inputs
     try:
         picked_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
         start = datetime.datetime.strptime(start_time, "%H:%M").time()
@@ -68,15 +66,49 @@ async def find(
 
         start_dt = datetime.datetime.combine(picked_date, start)
         end_dt = datetime.datetime.combine(picked_date, end)
-
-        view = CategoryView(picked_date, start_dt, end_dt)
-        await interaction.response.send_message(
-            "⛳️ Please choose one or more courses:",
-            view=view,
-            ephemeral=True
-        )
     except ValueError:
         await interaction.response.send_message("⚠️ Use YYYY-MM-DD for date and HH:MM for times.")
+
+    #data quality checks, you'd hope the user was smart enough :D
+    if start_dt <= datetime.datetime.now() or end_dt <= datetime.datetime.now():
+        await interaction.response.send_message("⚠️ Start and end time must be in the future.")
+        return
+    
+    if end_dt <= start_dt:
+        await interaction.response.send_message("⚠️ End time must be after start time.")
+        return
+    
+    if players < 1 or players > 4:
+        await interaction.response.send_message("⚠️ Players must be between 1 and 4.")
+        return
+    
+    #Creating a drop down view for course selection
+    view = CourseView(picked_date, start_dt, end_dt)
+    await interaction.response.send_message(
+        "⛳️ Please choose one or more courses:",
+        view=view,
+        ephemeral=True
+        )
+    
+    #Make sure user chooses courses or 60s timeout
+    timeout = await view.wait()
+
+    #get courses selected
+    courses = view.selected_courses
+    if courses:
+        await interaction.followup.send(
+            f"📅 Date: **{picked_date.strftime('%B %d, %Y')}**\n"
+            f"🕒 Start: **{start_dt.strftime('%H:%M')}**\n"
+            f"🕒 End: **{end_dt.strftime('%H:%M')}**\n"
+            f"⛳️ Courses: **{', '.join(courses)}**\n"
+            f"👥 Players: **{players}**",
+            ephemeral=True # ephemeral so only the user who sent the command sees it
+        )
+    else:
+        await interaction.followup.send(
+            "⚠️ You didn’t select any categories in time.",
+            ephemeral=True
+        )
 
 # --- Bot startup ---
 @bot.event
